b8a9fa52087a808801b7282d4400e45934b4469b
Brian Zhen <brianzhen1803@gmail.com>
2024-05-03 14:21:56 -0700

    added art and added array of themes

--- Assets/Script/GeneratePrompt.cs
+++ Assets/Script/GeneratePrompt.cs
@@ -15,6 +15,20 @@ public class GeneratePrompt : MonoBehaviour
     public string[] ratingLists = {"1", "2","2", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "5", "6", 
     "6", "6", "6", "6", "6", "6", "6", "6", "7", "7", "7", "7", "7", "7", "7", "7", "7", "8", "8", "8", "9", "9", "9", "10", "10", "10"};
 
+    public string[][] allPrompts;
+
+    public string[] looks = {};
+    public string[] social = {};
+    public string[] politics = {};
+    public string[] online = {};
+    public string[] videogames = {};
+    public string[] shows = {};
+    public string[] anime = {};
+    public string[] moneyrelated = {};
+    public string[] occupation = {};
+    public string[] intelligence = {};
+    public string[] books = {};
+
     // Start is called before the first frame update
     void Start() {
         rating.text = ratingLists[Random.Range(0,41)];
